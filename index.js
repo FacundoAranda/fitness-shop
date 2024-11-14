20cbf4bfe3dfe74f20b8459493dfcfccdcdb7b79
Facundo Aranda <facundoarandajoaquin@gmail.com>
2024-11-13 23:46:15 -0300

    Preparando el entorno de desarrollo

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-let JavaScript = 1;
+const express = require('express')
+const app = express()
+const PORT = 4000
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.listen(PORT, () => {
+    console.log(`App escuchando en puerto ${PORT}`)
+})
